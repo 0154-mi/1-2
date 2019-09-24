d8da692b3be1bbb09805755fe2bfe43e5894a636
0154-mi <54836910+0154-mi@users.noreply.github.com>
2019-09-24 16:29:14 +0800

    Create ComputExpression.java

---
+++ ComputExpression.java
@@ -0,0 +1,12 @@
+package ComputExpression;
+
+public class ComputExpression {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		System.out.println((10.5+2*3)/(45-3.5));
+
+	}
+
+}
