ed48e98135ff470b5bf3df091a97e5c8c364b2a2
0154-mi <54836910+0154-mi@users.noreply.github.com>
2019-09-24 16:40:14 +0800

    Create ShowSyntaxErrors.java

---
+++ ShowSyntaxErrors.java
@@ -0,0 +1,11 @@
+package ShowSyntaxErrors;
+
+public class ShowSyntaxErrors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java");
+
+	}
+
+}
