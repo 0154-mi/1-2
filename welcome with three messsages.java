541bbe5ea1894faa002ae94784c5d1f32f04b01f
0154-mi <54836910+0154-mi@users.noreply.github.com>
2019-09-24 16:22:47 +0800

    Create welcome with three messsages.java

---
+++ welcome with three messsages.java
@@ -0,0 +1,12 @@
+package welcomewiththreemessages;
+
+public class welcomewiththreemessages {
+
+	public static void main(String[] args) { 
+		// TODO Auto-generated method stub
+        System.out.println("Programming is fun!");
+        System.out.println("Funsamentals First");
+        System.out.println("Problem Driven");
+	}
+
+}
