6e167f6497ab4269b3fad71347d47b251162236b
0154-mi <54836910+0154-mi@users.noreply.github.com>
2019-10-09 20:29:14 +0800

    Create ShowLogicErrors.java

---
+++ ShowLogicErrors.java
@@ -0,0 +1,8 @@
+public class ShowLogicErrors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Celsius 35 is Fahreneit degree ");
+		System.out.println((9/5)*35+32);
+	}
+}
